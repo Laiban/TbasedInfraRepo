655ff9372b5edc3648e89fa3cedf4a7123c07a94
Isiaka Lukman <lukman.isiaka01@gmail.com>
2023-05-01 10:02:33 -0700

    Update backend.tf
    update the region. Take note:

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "infra-aws-tf-state-bucket"
     key = "main"
-    region = "us-east-1"
+    region = "us-west-2"
     dynamodb_table = "my-dynamo-db-table"
   }
 }
