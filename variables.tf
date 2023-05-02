eb028c41b6777e08ccf8917b5717a57670621e0c
Isiaka Lukman <lukman.isiaka01@gmail.com>
2023-05-02 09:49:24 -0700

    Update variables.tf
    Latest! Just change the  default key to null!

--- variables.tf
+++ variables.tf
@@ -5,8 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "new_keypair" 
-}
+    default     =  null}
 
 variable "instance_type" { 
     description = "instance type for ec2" 
